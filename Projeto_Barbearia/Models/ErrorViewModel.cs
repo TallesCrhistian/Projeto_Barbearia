7ed90889dbbe94c0abf2e2ba3d335041eb73bd17
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-07-21 23:39:07 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Barbearia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Barbearia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
